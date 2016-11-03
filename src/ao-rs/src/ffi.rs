0d0b2d5894e39b37d46f7f3a8f453fc2105d0530
Daniel Rivas <ers.daniel+dev@gmail.com>
2016-11-03 16:46:24 -0500

    Moved example to its own file

--- src/ao-rs/src/ffi.rs
+++ src/ao-rs/src/ffi.rs
@@ -1,17 +1,5 @@
 //! This module contains the ffi bindings for libao
-extern crate libc;
-
-use std::ptr;
-use std::f32::consts::{PI};
 use libc::{c_int, c_char};
-
-// bitflags! {
-//     flags AoByteFormat: c_int {
-//         const AO_FMT_LITTLE = 1,
-//         const AO_FMT_BIG = 2,
-//         const AO_FMT_NATIVE = 4,
-//     }
-// }
 
 const AO_FMT_LITTLE: c_int = 1;
 const AO_FMT_BIG: c_int = 2;
@@ -31,18 +19,6 @@ pub struct AoFormat {
     pub channels: c_int,
     pub byte_format: c_int,
     pub matrix: *mut c_char,
-}
-
-impl Default for AoFormat {
-    fn default() -> Self {
-        AoFormat {
-            bits: 16,
-            rate: 44100,
-            channels: 2,
-            byte_format: AO_FMT_LITTLE,
-            matrix: ptr::null_mut(),
-        }
-    }
 }
 
 /// Structure for libao's ao_option.
@@ -65,38 +41,3 @@ extern {
     pub fn ao_close(ao_device: *const AoDevice) -> c_int;
     pub fn ao_shutdown();
 }
-
-// fn main() {
-//     let driver = unsafe {
-//         ao_initialize();
-//         ao_default_driver_id()
-//     };
-
-//     let freq = 440.0;
-//     let mut format = AoFormat::default();
-
-//     let device = unsafe { ao_open_live(driver, &mut format, ptr::null_mut()) };
-
-//     // Creates buffer.
-//     let buff_size = (format.bits / 8 * format.channels * format.rate) as u32;
-//     let mut buffer: Vec<i8> = Vec::with_capacity(buff_size as usize);
-//     unsafe { buffer.set_len(buff_size as usize); }
-//     for (i, chunk) in buffer.chunks_mut(4).enumerate() {
-//         let sample = (0.75 * 32768.0 * (2.0 * PI * freq * (i as f32) / (format.rate as f32)).sin()) as i16;
-//         chunk[0] = (sample & 0xff) as i8;
-//         chunk[2] = (sample & 0xff) as i8;
-//         chunk[1] = ((sample >> 8) & 0xff) as i8;
-//         chunk[3] = ((sample >> 8) & 0xff) as i8;
-//         println!("{:?}", chunk);
-//     }
-
-//     let mut ret = -10;
-//     unsafe {
-//         ret = ao_play(device, buffer.as_mut_ptr(), buff_size);
-//         ao_close(device);
-//         ao_shutdown();
-//     }
-
-//     println!("Device: {:?}", device);
-//     println!("Ret: {:?}", buff_size);
-// }
