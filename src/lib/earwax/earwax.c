6dd7ad9a2ac139814af98b9327598347e28f4058
Daniel Rivas <ers.daniel+dev@gmail.com>
2017-01-29 17:05:59 -0600

    Fixed typo

--- src/lib/earwax/earwax.c
+++ src/lib/earwax/earwax.c
@@ -1,7 +1,7 @@
 /**
  * This file is supposed to be used by Rust's ffi bindings. It contains
  * simple yet useful functions for decoding any supported file format
- * into a stream of PCM data using ffmpeg 3.2
+ * into a stream of PCM data using ffmpeg 2.8.
  */
 
 #include <pthread.h>
